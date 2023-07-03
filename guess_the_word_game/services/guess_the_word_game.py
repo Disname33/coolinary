c906cc0aff7c01aa185ca4c22a76eac8e54d4662
DeNGeR <amokill@bk.ru>
2023-07-03 19:52:39 +0300

    version 2.8.6 ADD colored keyboard in guess_the_word

--- guess_the_word_game/services/guess_the_word_game.py
+++ guess_the_word_game/services/guess_the_word_game.py
@@ -70,14 +70,13 @@ def remove_line_with_word(word: str) -> str:
 def get_full_meaning_word(word: str) -> str:
     meaning_word = get_meaning_word(word)
     try:
+        next_word = None
         if " см. " in meaning_word:
             next_word = _find_next_word("см.", meaning_word)
-            if next_word is not None and next_word != 'также' and next_word != '':
-                return meaning_word + str(NounWithDefinition(noun=next_word, definition=get_meaning_word(next_word)))
-        elif " что: " in meaning_word:
+        elif " То же, что: " in meaning_word:
             next_word = _find_next_word("что:", meaning_word)
-            if next_word is not None and next_word != '':
-                return meaning_word + str(NounWithDefinition(noun=next_word, definition=get_meaning_word(next_word)))
+        if next_word is not None and next_word != 'также' and next_word != '':
+            return meaning_word + str(NounWithDefinition(noun=next_word, definition=get_meaning_word(next_word)))
     except Exception as e:
         print(e)
     return meaning_word
