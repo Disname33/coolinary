b3a144f1c7af89a0e4d103fd73ad871354b08193
DeNGeR <amokill@bk.ru>
2023-07-07 02:17:58 +0300

    version 3.0.2 Fix time at chat

--- coolinary/static/js/local-date.js
+++ coolinary/static/js/local-date.js
@@ -14,3 +14,12 @@ function localeDate(dateString) {
 
     return `${day} ${month} ${year} г.`;
 }
+
+function localeTime(dateString) {
+    const date = new Date(dateString);
+
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+    return `${hours}:${minutes}`;
+}
