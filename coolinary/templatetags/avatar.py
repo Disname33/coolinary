496aeedadd9d5e1b8b61b0190ddc19f08b4fc800
DeNGeR <amokill@bk.ru>
2023-10-04 15:12:44 +0300

    version 4.2.4 FIX bug at UserProfile

--- coolinary/templatetags/avatar.py
+++ coolinary/templatetags/avatar.py
@@ -1,11 +1,14 @@
 from django import template
 
+from coolinary.models import UserProfile
+
 register = template.Library()
 
 
 @register.filter
 def avatar(user):
-    if user.userprofile and user.userprofile.avatar:
-        return user.userprofile.avatar.url
+    profile = UserProfile.objects.get_or_create(user=user)[0]
+    if profile.avatar:
+        return profile.avatar.url
     else:
         return '/static/svg/avatar.svg'
