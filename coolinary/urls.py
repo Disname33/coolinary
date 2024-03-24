b0879cee7c61906efb43d53682f4f81b60109bc4
DeNGeR <amokill@bk.ru>
2024-03-24 22:37:17 +0300

    version 4.5.4 FIX original chatGPT

--- coolinary/urls.py
+++ coolinary/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -45,7 +44,7 @@ urlpatterns = [
     path('debt/', include('debt_app.urls'), name='debt'),
     path('i18n/', include('django.conf.urls.i18n'), name='lang'),
 ]
-urlpatterns += i18n_patterns(
-    path('chatGPT/', include('chatGPT.urls')),
-)
+# urlpatterns += i18n_patterns(
+#     path('chatGPT/', include('chatGPT.urls')),
+# )
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
