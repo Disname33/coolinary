be3f9261932b48f55ae16700f53015dda92bea34
DeNGeR <amokill@bk.ru>
2024-06-10 18:24:37 +0300

    version 4.6.9 Add clear button at guess helper

---
+++ games/static/tetris/send-score.js
@@ -0,0 +1,36 @@
+function showToast(text = 'Ошибка', title = 'Системное сообщение') {
+    const toastLive = document.getElementById('liveToast');
+    const toast = new bootstrap.Toast(toastLive);
+    toastLive.querySelector('.toast-title').textContent = title;
+    toastLive.querySelector('.toast-body').textContent = text;
+    toast.show();
+}
+
+function sendScore() {
+    if (account.score > 1000) {
+        const csrftoken2 = document.querySelector('[name=csrfmiddlewaretoken]').value;
+        $.ajax({
+            url: window.location.href,
+            type: 'GET',
+            headers: {'X-CSRFToken': csrftoken2},
+            data: {
+                'csrfmiddlewaretoken': csrftoken2,
+                'score': account.score,
+                'lines': account.lines,
+                'level': account.level,
+            },
+            success: function (response) {
+                // Обработка успешного ответа
+                console.log(response)
+                if (response === "OK") {
+                    showToast("Ваш результат добавлен в таблицу рекордов");
+                } else showToast("Войдите, чтобы Ваши результаты попадали в таблицу рекордов");
+                // setTimeout(toastBootstrap.hide(), 5000);
+            },
+            error: function (error) {
+                // Обработка ошибки
+                console.log(error);
+            }
+        });
+    }
+}
